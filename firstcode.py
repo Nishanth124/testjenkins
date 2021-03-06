2d4e0f1b62e7f0cd2999e07a9ee737dcb9bec5ac
Nishanth124 <60165619+Nishanth124@users.noreply.github.com>
2021-03-06 01:29:48 -0400

    py code

---
+++ firstcode.py
@@ -0,0 +1,26 @@
+number=5
+if number%2==0:
+    print("even")
+else:
+    print("odd")
+if  number % 2 == 0 and  number % 5 == 0 and  number %3 ==0:
+    print("it's divisible by 2,3 and5")
+elif number%2==0 and number%3==0 :
+       print("it's divisible by 2and3")
+elif number%3==0 and number%5==0:
+    print("it's divisible by 3and5")
+elif number % 2 == 0 and  number % 5 == 0:
+        print("it's divisible by 2and5")
+
+
+elif number%2==0:
+    print("it's divisible by 2")
+elif number % 5 == 0:
+      print("it's divisible by 5")
+elif number%3==0:
+      print("it's divisible by 3")
+
+
+
+
+
